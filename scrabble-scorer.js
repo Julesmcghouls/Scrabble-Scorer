980e063f33fd4b4b45c2148f79accf69cb0dd7c9
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-02-06 21:13:41 -0600

    Refactor scrabble scorer function

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -3,37 +3,40 @@
 const input = require("readline-sync");
 
 const oldPointStructure = {
-  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-  2: ['D', 'G'],
-  3: ['B', 'C', 'M', 'P'],
-  4: ['F', 'H', 'V', 'W', 'Y'],
-  5: ['K'],
-  8: ['J', 'X'],
-  10: ['Q', 'Z']
+1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+2: ['D', 'G'],
+3: ['B', 'C', 'M', 'P'],
+4: ['F', 'H', 'V', 'W', 'Y'],
+5: ['K'],
+8: ['J', 'X'],
+10: ['Q', 'Z']
 };
 
 function oldScrabbleScorer(word) {
-	word = word.toUpperCase();
-	let letterPoints = "";
- 
-	for (let i = 0; i < word.length; i++) {
- 
-	  for (const pointValue in oldPointStructure) {
- 
-		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
-		 }
- 
-	  }
-	}
-	return letterPoints;
- }
+word = word.toUpperCase();
+let letterPoints = "";
+
+for (let i = 0; i < word.length; i++) {
+
+   for (const pointValue in oldPointStructure) {
+
+      if (oldPointStructure[pointValue].includes(word[i])) {
+      letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+      }
+
+   }
+}
+return letterPoints;
+}
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+console.log("Let's play some scrabble!");
+let word = input.question("Enter a word to score: ");
+let score = oldScrabbleScorer(word);
+console.log(`Score for ${word}: ${score}`);
 };
 
 let simpleScorer;
@@ -51,21 +54,21 @@ function transform() {};
 let newPointStructure;
 
 function runProgram() {
-   initialPrompt();
-   
+initialPrompt();
+
 }
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
-   initialPrompt: initialPrompt,
-   transform: transform,
-   oldPointStructure: oldPointStructure,
-   simpleScorer: simpleScorer,
-   vowelBonusScorer: vowelBonusScorer,
-   scrabbleScorer: scrabbleScorer,
-   scoringAlgorithms: scoringAlgorithms,
-   newPointStructure: newPointStructure,
-	runProgram: runProgram,
-	scorerPrompt: scorerPrompt
+initialPrompt: initialPrompt,
+transform: transform,
+oldPointStructure: oldPointStructure,
+simpleScorer: simpleScorer,
+vowelBonusScorer: vowelBonusScorer,
+scrabbleScorer: scrabbleScorer,
+scoringAlgorithms: scoringAlgorithms,
+newPointStructure: newPointStructure,
+runProgram: runProgram,
+scorerPrompt: scorerPrompt
 };
